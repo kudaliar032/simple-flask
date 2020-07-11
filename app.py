154693c2a3801d0ac8f0840b7896d2a1c5c72e45
Aditya Rahman <adityarahman032@gmail.com>
2020-07-11 12:52:12 +0800

    initial commit :id:

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+  return 'Hello, this simple Flask!'
+
+
+@app.route('/other')
+def other_page():
+  return 'Other flask page!'
+
+
+if __name__ == '__main__':
+  app.run()
